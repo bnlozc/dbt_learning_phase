7c9f3ac7c8771e7cc6e1a59a9e154ef9607cb43a
Benal <benal.clk@gmail.com>
2025-11-09 15:08:51 +0000

    golden layer is added - mart

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -3,7 +3,7 @@ with orders_per_day as(
         date_date
         ,count(distinct orders_id) as nb_transactions
         ,round(sum(revenue),0) as revenue
-        ,round(sum,(margin),0) as margin
+        ,round(sum(margin),0) as margin
         ,ROUND(SUM(operational_margin),0) AS operational_margin
         ,ROUND(SUM(purchase_cost),0) AS purchase_cost
         ,ROUND(SUM(shipping_fee),0) AS shipping_fee
@@ -17,12 +17,12 @@ select
 date_date
 ,revenue
 ,margin
-,operational_cost
+,operational_margin
 ,purchase_cost
-shipping_fee
+,shipping_fee
 ,log_cost
 ,ship_cost
 ,quantity
-, ROUND(revenue/NULLIF(nb_transactions, 0), 2) AS average_basket
+,ROUND(revenue/NULLIF(nb_transactions, 0), 2) AS average_basket
 from orders_per_day
 order by date_date desc
